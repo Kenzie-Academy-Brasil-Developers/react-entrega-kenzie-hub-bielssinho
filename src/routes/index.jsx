8bda7634a768f0fe6b31e13db584688d3c5a5c36
bielssinho <bielnmjr@gmail.com>
2022-11-28 15:53:31 -0300

    First commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+
+
+export const RoutesMain = () => {
+    return(
+        <Routes>
+            <Route/>
+            <Route/>
+            <Route/>
+            <Route/>
+        </Routes>
+    )
+}
