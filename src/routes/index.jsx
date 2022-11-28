368ef7f08f62c6642a977fde0723bed8455c6798
bielssinho <bielnmjr@gmail.com>
2022-11-28 19:51:50 -0300

    Feat: create forms

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
-
+import { DivNotFound } from '../components/NotFound/notfound';
+import { Dashboard } from '../pages/Dashboard';
+import { Login } from '../pages/Login';
+import { Register } from '../pages/Register';
 
 export const RoutesMain = () => {
     return(
         <Routes>
-            <Route/>
-            <Route/>
-            <Route/>
-            <Route/>
+            <Route path='/login' index element={<Login/>}/>
+            <Route path='/register' element={<Register/>}/>
+            <Route path='/dashboard' element={<Dashboard/>}/>
+            <Route path='*' element={<DivNotFound>404 NOT FOUND</DivNotFound>}/>
         </Routes>
     )
 }
